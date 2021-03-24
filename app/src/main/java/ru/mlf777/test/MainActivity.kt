61d5adcc31f0996a98f4da155f00d4e57c24e9ae
Alex <alex-test@mail.ru>
2021-03-24 16:38:22 +0300

    Initial commit

---
+++ app/src/main/java/ru/mlf777/test/MainActivity.kt
@@ -0,0 +1,32 @@
+package ru.mlf777.test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import kotlin.reflect.KProperty
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
+        return "$thisRef, спасибо за делегирование мне '${property.name}'!"
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
+        println("POINT_0 $value было присвоено значению '${property.name} в $thisRef.'")
+    }
+}
+
+class MainActivity : AppCompatActivity() {
+    var p: String by Delegate()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        p = "NEW"
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            Log.d("POINT_0", "test $p")
+        }
+    }
+}
